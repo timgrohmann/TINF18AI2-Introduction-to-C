5dd0d555faa785def494e898d9423c18d04f8e4c
timgrohmann <timgrohmann@icloud.com>
2018-10-02 09:44:26 +0200

    Initial Commit

---
+++ Introduction_to_C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Introduction_to_C
+//
+//  Created by Tim Grohmann on 02.10.18.
+//  Copyright © 2018 Tim Grohmann. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
